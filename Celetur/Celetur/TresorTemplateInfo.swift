4420849156cc0b9d7db12972d23c3de0ea1926dd
Hugo Schlecken <h.schlecken@gmx.de>
2018-01-07 18:52:16 +0100

    refactor payload metadaten

---
+++ Celetur/Celetur/TresorTemplateInfo.swift
@@ -0,0 +1,44 @@
+//
+//  TresorAppState.swift
+//  Celetur
+//
+//  Created by Feldmaus on 07.08.17.
+//  Copyright © 2017 ischlecken. All rights reserved.
+//
+
+import Foundation
+import CeleturKit
+import CloudKit
+
+class TresorTemplateInfo {
+  
+  var templatenames : [String] = []
+  
+  fileprivate var templates : [String: PayloadMetainfo] = [:]
+  
+  init() {
+    let templateUrls = Bundle.templateURLs()
+    
+    for u in templateUrls {
+      if let p = PayloadSerializer.payloadMetainfo(jsonUrl: u) {
+        
+        self.templatenames.append(p.name)
+        self.templates[p.name] = p
+        
+      }
+    }
+  }
+  
+  
+  public func payloadMetainfoItem(name: String, indexPath: IndexPath) -> PayloadMetainfoItem? {
+    guard let t = self.templates[name],
+      indexPath.section < t.sections.count && indexPath.row < t.sections[indexPath.section].items.count
+      else { return nil }
+    
+    return t.sections[indexPath.section].items[indexPath.row]
+  }
+  
+  public func payloadMetainfo(name: String) -> PayloadMetainfo? {
+    return self.templates[name]
+  }
+}
